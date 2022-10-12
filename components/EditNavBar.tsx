b518b324aeacc454f8164a13c36c8b86c6858602
Donahue Avila <d@donahue.dev>
2022-10-12 17:31:41 -0500

    fixed logout

--- components/EditNavBar.tsx
+++ components/EditNavBar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
         {/* <a className="mr-4" href="./settings">Account</a> */}
         <a className="mx-2" href="./links">Links</a>
         <a className="mx-2" href="./themes">Themes</a>
-        <div onClick={()=>{fetch("./api/users/logout"), {method: "POST"};window.location.replace("/");}} className="font-semibold cursor-pointer mx-2">Logout</div>
+        <div onClick={()=>{fetch("/api/users/logout", {method: "POST"}), window.location.replace("/");}} className="font-semibold cursor-pointer mx-2">Logout</div>
       </div>
     
       <hr />
