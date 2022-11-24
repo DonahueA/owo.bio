1d83b84b94b810035032479c7a6a8b13d9942a80
Donahue Avila <donahue.avila@gmail.com>
2022-11-24 04:04:20 -0600

    Upgraded to nextjs 13

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,8 +16,8 @@ const Home: NextPage = () => {
             font-family: Inter;
           }`}</style>
       <div className="pt-48 pb-24"><h1 className="text-4xl font-bold">{isHovered ? "owo" : "uwu"}</h1></div>
-      <div style={{marginBottom: "16px"}}><Link href="/login"><a className='button wide' onMouseEnter={()=>setIsHovered(true)} onMouseLeave={()=>setIsHovered(false)}>login</a></Link></div>
-      <div><Link href="/register"><a className='button wide'>make your own page</a></Link></div>
+      <div style={{marginBottom: "16px"}}><Link href="/login" className='button wide' onMouseEnter={()=>setIsHovered(true)} onMouseLeave={()=>setIsHovered(false)}>login</Link></div>
+      <div><Link href="/register" className='button wide'>make your own page</Link></div>
     </Layout>
     )
 
